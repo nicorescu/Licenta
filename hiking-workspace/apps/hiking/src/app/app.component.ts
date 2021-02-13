a0f14b386699acaa47da5d2b85f5a0225fd1a8e6
Teodor Nicorescu <59024360+nicorescu@users.noreply.github.com>
2021-02-13 19:21:29 +0200

    created app

---
+++ hiking-workspace/apps/hiking/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hiking';
+}
---
+++ hiking-workspace/libs/hiking/data-access/README.md
@@ -0,0 +1,7 @@
+# hiking-data-access
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test hiking-data-access` to execute the unit tests.
