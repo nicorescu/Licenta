9098ccc56037db4ac67ee4d8e12bbe37aff26740
Nicorescu Teodor Ionut <teodor_nicorescu@trimble.com>
2021-02-23 17:11:19 +0200

    API

--- TripService/TripService/Startup.cs
+++ TripService/TripService/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HikingService
+namespace TripService
 {
     public class Startup
     {
--- TripService/TripService/WeatherForecast.cs
+++ TripService/TripService/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace HikingService
+namespace TripService
 {
     public class WeatherForecast
     {
