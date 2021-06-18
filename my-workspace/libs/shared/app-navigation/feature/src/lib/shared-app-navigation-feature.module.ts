e0f1af7538e94e36789fa73446e685e0ef97eb66
Nicorescu Teodor Ionut <teodor_nicorescu@trimble.com>
2021-06-18 22:43:24 +0300

    documentatie

--- my-workspace/libs/shared/app-navigation/feature/src/lib/shared-app-navigation-feature.module.ts
+++ my-workspace/libs/shared/app-navigation/feature/src/lib/shared-app-navigation-feature.module.ts
@@ -3,40 +3,38 @@ import { CommonModule } from '@angular/common';
 import { SideNavigationMenuComponent } from './components/side-navigation-menu/side-navigation-menu.component';
 import { SideNavigationInnerToolbarComponent } from './containers/side-navigation-inner-toolbar/side-navigation-inner-toolbar.component';
 import { DxDrawerModule } from 'devextreme-angular/ui/drawer';
-import { DxScrollViewModule} from 'devextreme-angular/ui/scroll-view';
+import { DxScrollViewModule } from 'devextreme-angular/ui/scroll-view';
 import { DxToolbarModule } from 'devextreme-angular/ui/toolbar';
 import { DxLoadIndicatorModule } from 'devextreme-angular/ui/load-indicator';
 import { DxButtonModule } from 'devextreme-angular/ui/button';
-import { DxTreeViewModule} from 'devextreme-angular/ui/tree-view';
+import { DxTreeViewModule } from 'devextreme-angular/ui/tree-view';
 import { DxListModule } from 'devextreme-angular/ui/list';
 import { DxContextMenuModule } from 'devextreme-angular/ui/context-menu';
 import { ScreenService } from './options/screen.service';
 import { TranslocoModule } from '@ngneat/transloco';
 import { TranslocoLocaleModule } from '@ngneat/transloco-locale';
-import {HikingUiUiCommonUiModule} from '@hkworkspace/hiking-ui/ui-common/ui';
+import { HikingUiUiCommonUiModule } from '@hkworkspace/hiking-ui/ui-common/ui';
 
 @NgModule({
   imports: [
     CommonModule,
-    DxTreeViewModule,
+    TranslocoLocaleModule.init(),
     DxDrawerModule,
     DxScrollViewModule,
-    DxButtonModule,
     DxToolbarModule,
-    DxListModule,
     DxLoadIndicatorModule,
+    DxButtonModule,
+    DxTreeViewModule,
+    DxListModule,
     DxContextMenuModule,
     TranslocoModule,
     HikingUiUiCommonUiModule,
-    TranslocoLocaleModule.init()
   ],
-  providers: [
-    ScreenService
-  ],
+  providers: [ScreenService],
   declarations: [
     SideNavigationMenuComponent,
-    SideNavigationInnerToolbarComponent
+    SideNavigationInnerToolbarComponent,
   ],
-  exports: [SideNavigationInnerToolbarComponent]
+  exports: [SideNavigationInnerToolbarComponent],
 })
 export class SharedAppNavigationFeatureModule {}
